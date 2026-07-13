bcd844d5c25624be7fadc0c482b167758dc19586
Neergrevir <rglee0127@gmail.com>
2026-07-13 21:55:58 +0900

    Delete run_mac_linux.sh

--- run_mac_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "$0")"
-echo "[1/2] 필요한 라이브러리를 설치합니다..."
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-echo "[2/2] Streamlit 앱을 실행합니다..."
-python3 -m streamlit run streamlit_app.py
